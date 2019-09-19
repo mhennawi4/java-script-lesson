77fb3b08dc6cce297733e182a65cac2a0ba7ded6
mahmoud <m.hennawi4@share.epsb.ca>
2019-09-19 13:44:32 -0600

    update

---
+++ scripts/introlessons/operators.js
@@ -0,0 +1,22 @@
+//console.log("did you change my file name?");
+console.log("you made it to the console!");
+
+function simpleoperators (numberA, numberB)  {
+  let adding = numberA + numberB;
+  let subtraction =  numberA - numberB;
+  let mulitplying = numberA * numberB;
+  let division =  numberA / numberB;
+  let modulus = numberA % numberB;
+  console.log(numberA, numberB, 'adding' adding);
+  console.log(numberA, numberB, 'subtraction' subtraction);
+  console.log(numberA, numberB, 'mulitplying' mulitplying);
+  console.log(numberA, numberB, 'division' division);
+  console.log(numberA, numberB, 'modulus' modulus);
+}
+
+let number = [4, 7, 53, 69, 19, 0 ,100 , 1, '1']
+
+// simpleoperators
+simpleoperators(number[0], number[1])
+simpleoperators(number[2], number[3])
+simpleoperators(number[4], number[5])
