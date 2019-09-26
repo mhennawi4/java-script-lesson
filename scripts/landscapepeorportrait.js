c2523f2f5aee1c9573b3248c3302b9cafc5356a8
mahmoud <m.hennawi4@share.epsb.ca>
2019-09-26 13:40:08 -0600

    update

--- scripts/landscapepeorportrait.js
+++ scripts/landscapepeorportrait.js
@@ -1,2 +1,16 @@
 //console.log("Did you change my file name?");
-console.log("You made it to the console!");
+//console.log("You made it to the console!");
+
+function main() {
+  console.log("it works");
+
+  let firstnumber, secondnumber, text;
+
+  //get the width-value from id=textfield1
+  firstnumber= document.getelementbyid("textfield1").value;
+  document.getelementbyid("validityTest1").innerHTML = testNaN (firstnumber);
+//testNaN (firstnumber);
+
+  //get the height-value from id=textfield2
+  secondnumber= document.getelementbyid("textfield2").value;
+}
