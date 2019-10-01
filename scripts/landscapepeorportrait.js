764ebdec0045727a7fb0b8658480631cc709b4a1
mahmoud <m.hennawi4@share.epsb.ca>
2019-10-01 13:53:59 -0600

    update

--- scripts/landscapepeorportrait.js
+++ scripts/landscapepeorportrait.js
@@ -1,10 +1,10 @@
 //console.log("Did you change my file name?");
-//console.log("You made it to the console!");
+console.log("You made it to the console!");
 
-let stop = false; //NAN Boolean Memory
+let stop = false;
 
 function testNaN (number) {
-  //console.log(stop);
+
   if ( isNaN (number) ) {
     stop = true;
     return "Type a real number"
@@ -13,10 +13,9 @@ function testNaN (number) {
   }
 }
 
-function geometry (width, height) { //Notice these parameters are local and do not mix with "number" variable
-  if (width == height) return 'Square'; //must be first, return will not allow rest of code to run
-  return (width > height) ? 'Landscape' : 'Portrait'; //ternary operator example
-  //Seems odd, the inequality, here, is reading the opposite
+function geometry (width, height) {
+  if (width == height) return 'Square';
+  return (width > height) ? 'Landscape' : 'Portrait';
 }
 
 function checkboxes() {
@@ -41,25 +40,21 @@ function checkboxes() {
   }
 }
 
-// This coding illustrates the advanced method
 function main() {
-  //console.log("It works");
-
-  //checkboxes();
 
 
   let firstNumber, secondNumber, text;
 
-  //Get the Width-value from id=textfield1
+
   firstNumber = document.getElementById('textfield1').value;
 
-  //testNaN (firstNumber);
+
   document.getElementById('validityTest1').innerHTML = alert ( testNaN (firstNumber));
   document.getElementById('validityTest1').innerHTML = testNaN (firstNumber);
 
-  //Get the Height-value from id=textfield2
+
   secondNumber = document.getElementById('textfield2').value;
-  //testNaN (secondNumber);
+
   document.getElementById('validityTest2').innerHTML = alert ( testNaN (secondNumber));
   document.getElementById('validityTest2').innerHTML = testNaN (secondNumber);
 
